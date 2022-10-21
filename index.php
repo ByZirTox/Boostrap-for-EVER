963a16039e760d3923bef57b5e854ef0fea1abb1
ByZirTox <xxtedyxxrenard@gmail.com>
2022-10-21 11:01:25 +0200

    fefef git commit -m fefef
    git commit -m fefef

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css" />
+  <title>Boostrap for ever</title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Discovering Bootstraps 5.1</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categories
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Photos</a></li>
+              <li><a class="dropdown-item" href="#">Articles</a></li>
+              <li><a class="dropdown-item" href="#">Projects</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <h1>Journey into Boostrap classes !</h1>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <img src="Gentau_Pic_du_Midi_Ossau.jpg" alt="lake" height="250px" width="300px">
+        <h2> Visit Components lake </h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque obcaecati modi corrupti qui temporibus doloremque aliquid hic necessitatibus deserunt inventore sequi eveniet, laudantium a at veniam. Aliquid voluptas libero nulla?</p>
+        <button class="container">Read more</button>
+      </div>
+      <div class="col-md-3">
+        <img src="cascade.jpg" alt="lake" height="250px" width="300px">
+        <h2> Facing Cascade style sheet </h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque obcaecati modi corrupti qui temporibus doloremque aliquid hic necessitatibus deserunt inventore sequi eveniet, laudantium a at veniam. Aliquid voluptas libero nulla?</p>
+        <button class="container">Read more</button>
+      </div>
+      <div class="col-md-3">
+        <img src="riviereFrancephotodouverture-1024x668-1.jpg" alt="lake" height="250px" width="300px">
+        <h2> Contemplate form inputs river</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque obcaecati modi corrupti qui temporibus doloremque aliquid hic necessitatibus deserunt inventore sequi eveniet, laudantium a at veniam. Aliquid voluptas libero nulla?</p>
+        <button class="container">Read more</button>
+      </div>
+      <div class="col-md-3">
+        <img src="index.jpeg" alt="lake" height="250px" width="300px">
+        <h2> Enjoy responsive layout land </h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque obcaecati modi corrupti qui temporibus doloremque aliquid hic necessitatibus deserunt inventore sequi eveniet, laudantium a at veniam. Aliquid voluptas libero nulla?</p>
+        <button class="container">Read more</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 bg-light">
+        <h3>The css frameworks are very useful</h3>
+        <p>Learning them commes with practice</p>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-13 bg-primary">
+        <footer> @2022 wilders / image @picsum</footer>
+      </div>
+    </div>
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
